7ec232265859197529a17b3dbd0f55fcd97693f1
Chenxinyu Bao <admin001@sjtu.edu.cn>
2025-02-26 11:02:59 +0800

    NEW: wifi: add wifi file & test

---
+++ main/component_tests/wifi_test.c
@@ -0,0 +1,12 @@
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "esp_system.h"
+#include "esp_spi_flash.h"
+#include "esp_log.h"
+#include "esp_wifi.h"
+#include "nvs_flash.h"
+
+void app_main(void) {
+    
+    return ;
+}

a2d7b286955a007764c134521ea045994706185d
crosshill <crosshill@sjtu.edu.cn>
2025-02-26 21:00:45 +0800

    NEW:espfilght:refined file structures

--- main/main.c
+++ main/main.c
@@ -1,5 +1,24 @@
+<<<<<<< HEAD
 
 void app_main(void) {
 
     return ;
 }
+=======
+#include "driver/i2c_master.h"
+#include "driver/gpio.h"
+#include "esp_err.h"
+#include "esp_log.h"
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include <string.h>
+#include <stdio.h>
+#include <pid.h>
+#define TAG "EspFlight"
+
+void app_main(void)
+{
+    ESP_LOGI(TAG, "Initialize LVGL");
+    func();
+}
+>>>>>>> 5f6b4e8 (NEW:espfilght:refined file structures)
